97480fd67aa290f73e90ac8b11f0cd6a945339fb
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-19 22:13:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+from telethon import TelegramClient
+import logging
+import os
+import config 
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('logs.txt'),
+              logging.StreamHandler()],
+    level=logging.INFO)
+
+
+
+
+tbot = TelegramClient("logomakerbot", API_ID, API_HASH)
+
+
+
+def register(**args):
+    """ Registers a new message. """
+    pattern = args.get('pattern', None)
+
+    r_pattern = r'^[/!]'
+
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
+
+    args['pattern'] = pattern.replace('^/', r_pattern, 1)
+
+    def decorator(func):
+        telethn.add_event_handler(func, events.NewMessage(**args))
+        return func
+
+    return decorator
