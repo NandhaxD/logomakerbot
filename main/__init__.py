f6d6f035ab98c67dc3f5bd20babfae27e240df3e
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-20 10:00:11 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -19,7 +19,6 @@ LOGO_API_URL1 = "https://techzbotsapi.herokuapp.com/logo?text="
 LOGO_API_URL2 = "https://techzbotsapi.herokuapp.com/logo?square=true&text="
 
 #Cilent start
-pyrobot = Client("logomaker", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)   
-      
-print("[INFO]: STARTING BOT...")
+bot = Client("nandhabot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(__name__)))
+
 pyrobot.run()
