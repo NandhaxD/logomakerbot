9d52ddad9c71731c4adaa6bfe04706908be573db
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-20 11:21:20 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -1,6 +1,7 @@
 from main import pyrobot, support 
+from pyrogram import __version__ as pyrover
 
 if __name__ == "__main__":
    pyrobot.run()
    with pyrobot:
-      pyrobot.send_message(f"@{support}",message="yeah")
+      pyrobot.send_message(f"@{support}",f"Iam online Now!\nPyroverion: {pyrover}")
