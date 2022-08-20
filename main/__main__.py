baf093db01662378342ba9a5c7f949e6984b50ad
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-20 10:03:30 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -3,4 +3,4 @@ from main import pyrobot, support
 if __name__ == "__main__":
    pyrobot.run()
    with pyrobot:
-      await pyrobot.send_message(f"@{support}",message="yeah")
+      pyrobot.send_message(f"@{support}",message="yeah")
