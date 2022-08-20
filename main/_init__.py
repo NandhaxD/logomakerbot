179562efa0c1ec359eedade43c64d5a1fa2282c0
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-20 09:01:39 +0530

    Update and rename __main__.py to _init__.py

--- main/_init__.py
+++ main/_init__.py
@@ -2,11 +2,14 @@ from pyrogram import filters, Client
 from pyrogram.types import *
 import os
 from requests import get
+import aiohttp
 
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
+print("[INFO]: STARTING AIOHTTP CLIENT")
+session = aiohttp.ClientSession()
 
 #logo api's
 LOGO_API_URL1 = "https://techzbotsapi.herokuapp.com/logo?text="
@@ -15,4 +18,5 @@ LOGO_API_URL2 = "https://techzbotsapi.herokuapp.com/logo?square=true&text="
 #Cilent start
 pyrobot = Client("logomaker", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)   
       
-pyrobot.run()
+print("[INFO]: STARTING BOT...")
+app.start()
