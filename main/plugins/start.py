a5f6b869ee09c55996e5a641899d32dd600fb5f8
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-20 11:59:32 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -3,6 +3,7 @@ from pyrogram.types import *
 from main import pyrobot, support, updates
 
 START_text = """**Hello,** {}
+
 **Iam A LogomakingBot I can make logo for you 
 
 use: /logo name | for normal logos. 
@@ -15,5 +16,6 @@ Buttons = [[ InlineKeyboardButton(text="Support", url=f"t.me/{support}"),
   
 @pyrobot.on_message(filters.command("start"))
 async def start(_, message):
-      await message.reply_photo(photo="https://telegra.ph/file/f1958a5500458504879a7.jpg",
+      logomakerbot = "https://telegra.ph/file/f1958a5500458504879a7.jpg"
+      await message.reply_photo(photo=logomakerbot,
       START_text.format(message.from_user.mention),reply_markup=InlineKeyboardMarkup(Buttons))
